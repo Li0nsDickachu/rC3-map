cf27886c0b25a1984fcc4041ce456db88945ccf2
Li0nsDickachu <li0nsdickachu@outlook.com>
2020-12-27 07:21:38 +0000

    Deploying to gh-pages from master @ 2ace435bc63a89a383fcc640672e135eff0a0633 🚀

---
+++ mindustry_tiles/power/mindustry-blocks-power.tsx
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="mindustry-power" tilewidth="128" tileheight="128" tilecount="39" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="96" height="96" source="battery-large-top.png"/>
+ </tile>
+ <tile id="1">
+  <image width="96" height="96" source="battery-large.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="battery-top.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="battery.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="combustion-generator-top.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="combustion-generator.png"/>
+ </tile>
+ <tile id="6">
+  <image width="96" height="96" source="differential-generator-liquid.png"/>
+ </tile>
+ <tile id="7">
+  <image width="96" height="96" source="differential-generator-top.png"/>
+ </tile>
+ <tile id="8">
+  <image width="96" height="96" source="differential-generator.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="diode-arrow.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="diode.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="illuminator-top.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="illuminator.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="impact-reactor-bottom.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="impact-reactor-light.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="impact-reactor-plasma-0.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="impact-reactor-plasma-1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="impact-reactor-plasma-2.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="impact-reactor-plasma-3.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="impact-reactor.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="power-node-large.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="power-node.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="power-source.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="power-void.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="rtg-generator-top.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="rtg-generator.png"/>
+ </tile>
+ <tile id="26">
+  <image width="96" height="96" source="solar-panel-large.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="solar-panel.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="surge-tower.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="thermal-generator.png"/>
+ </tile>
+ <tile id="30">
+  <image width="96" height="96" source="thorium-reactor-lights.png"/>
+ </tile>
+ <tile id="31">
+  <image width="96" height="96" source="thorium-reactor-top.png"/>
+ </tile>
+ <tile id="32">
+  <image width="96" height="96" source="thorium-reactor.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="turbine-generator-cap.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="turbine-generator-liquid.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="turbine-generator-top.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="turbine-generator-turbine0.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="turbine-generator-turbine1.png"/>
+ </tile>
+ <tile id="38">
+  <image width="64" height="64" source="turbine-generator.png"/>
+ </tile>
+</tileset>
