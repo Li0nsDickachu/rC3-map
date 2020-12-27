af5dbf9b2d8cf40401fb11aff34e365ccb786221
Li0nsDickachu <li0nsdickachu@outlook.com>
2020-12-27 03:52:24 +0000

    Deploying to gh-pages from master @ 5c7e5cc752fffbb7c94f473aa4da94092542200b 🚀

---
+++ mindustry_tiles/logic/mindustry-blocks-logic.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="mindustry-blocks-logic" tilewidth="96" tileheight="96" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="96" height="96" source="data-processor-top.png"/>
+ </tile>
+ <tile id="1">
+  <image width="96" height="96" source="logic-display.png"/>
+ </tile>
+ <tile id="2">
+  <image width="96" height="96" source="logic-processor-3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="logic-processor.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="memory-cell.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="message.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="micro-processor.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="switch-on.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="switch.png"/>
+ </tile>
+</tileset>
---
+++ mindustry_tiles/storage/mindustry-blocks-storage.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="mindustry-storage" tilewidth="160" tileheight="160" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="45">
+  <image width="96" height="96" source="vault.png"/>
+ </tile>
+ <tile id="46">
+  <image width="32" height="32" source="unloader.png"/>
+ </tile>
+ <tile id="47">
+  <image width="32" height="32" source="unloader-center.png"/>
+ </tile>
+ <tile id="48">
+  <image width="96" height="96" source="core-shard.png"/>
+ </tile>
+ <tile id="49">
+  <image width="96" height="96" source="core-shard-team.png"/>
+ </tile>
+ <tile id="50">
+  <image width="160" height="160" source="core-nucleus.png"/>
+ </tile>
+ <tile id="51">
+  <image width="160" height="160" source="core-nucleus-team.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="core-foundation.png"/>
+ </tile>
+ <tile id="53">
+  <image width="128" height="128" source="core-foundation-team.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="64" source="container.png"/>
+ </tile>
+</tileset>
